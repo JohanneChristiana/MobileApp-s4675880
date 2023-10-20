b12dbbf2f50c564568ae2cf25b64b1077e8ff445
Yohan <johannemenez22@gmail.com>
2023-10-20 16:51:32 +1100

    Added variables for button (Username and Password), also initialized button and added third party dependencies for retrofit

--- app/src/main/java/com/example/mobileapp_s4675880/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp_s4675880/MainActivity.kt
@@ -3,30 +3,57 @@ package com.example.mobileapp_s4675880
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     // Variable for Button
-    private var button: AppCompatButton? = null
+    private var usernameInput = ""
+    private var passwordInput = ""
 
-    //Intent pt.1
-    private val intentToNavigateToSecondScreen by lazy {
-        Intent(this, SecondScreenActivity::class.java)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // Layout Resource
 
         // Initialize Button
-        button = findViewById(R.id.btnSecondScreen) as? AppCompatButton;
-
-        button?.setOnClickListener {
-            Log.d("App", "Button Clicked")
-
-            //Intent pt.2
-            startActivity(intentToNavigateToSecondScreen)
+        findViewById<EditText>(R.id.usernameInput).addTextChangedListener {
+            usernameInput = it.toString()
         }
 
+        findViewById<EditText>(R.id.passwordInput).addTextChangedListener {
+            passwordInput = it.toString()
+        }
+
+        // Initialize Button
+        findViewById<EditText>(R.id.btnSecondScreen).setOnClickListener {
+            //Retrofit call here
+
+
+        }
     }
 }
+
+
+//    //Intent pt.1
+//    private val intentToNavigateToSecondScreen by lazy {
+//        Intent(this, SecondScreenActivity::class.java)
+//    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main) // Layout Resource
+//
+//        // Initialize Button
+//        button = findViewById(R.id.btnSecondScreen) as? AppCompatButton;
+//
+//        button?.setOnClickListener {
+//            Log.d("App", "Button Clicked")
+//
+//            //Intent pt.2
+//            startActivity(intentToNavigateToSecondScreen)
+//        }
+//
+//    }
+//}
