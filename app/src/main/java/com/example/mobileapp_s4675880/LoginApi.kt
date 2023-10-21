6c8e70422aca9b02720eba59f5d814569585b854
Yohan <johannemenez22@gmail.com>
2023-10-21 13:46:23 +1100

    Added LoginApi, and Retrofit is working

--- app/src/main/java/com/example/mobileapp_s4675880/LoginApi.kt
+++ app/src/main/java/com/example/mobileapp_s4675880/LoginApi.kt
@@ -1,7 +1,11 @@
 package com.example.mobileapp_s4675880
 
+import retrofit2.http.GET
+import retrofit2.http.Query
 interface LoginApi {
 
+    // When login function is called, we pass the queries values to the endpoint GET"login"
     @GET("login")
-    suspend fun login(): LoginResponse
+    suspend fun login(@Query("username") username:String, @Query("password") password:String): LoginResponse
+    // Remember Values 'username' and 'password' should have the same value as IntelliJ IDEA
 }
