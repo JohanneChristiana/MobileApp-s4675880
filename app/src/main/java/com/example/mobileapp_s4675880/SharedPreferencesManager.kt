f1f767ed5d33cf3e3a851cf3a17406ebaa2047a9
Yohan <johannemenez22@gmail.com>
2023-10-24 18:16:09 +1100

    SharedPreferences not working

--- app/src/main/java/com/example/mobileapp_s4675880/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/mobileapp_s4675880/SharedPreferencesManager.kt
@@ -1,2 +1,2 @@
 package com.example.mobileapp_s4675880
-class SharedPreferencesManager(private val sharedPreferences: String)
+class SharedPreferencesManager(private val sharedPreferences: String, password: String)
